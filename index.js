c1b95779111b9a36c7e7ade9c4d0bb81b226ec5c
Abdul Aziz <abdulaziz758015@gmail.com>
2025-05-31 10:58:15 +0600

    added some code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express')
 const cors =require('cors')
 const app = express()
 const port =process.env.PORT || 3000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
@@ -25,6 +25,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollection =client.db('JobPortal').collection('Job');
+    const applicationsCollection =client.db('JobPortal').collection('application')
+
+    //jobs app
+
+    app.get('/Job',async(req,res)=>{
+        const cursor =jobsCollection.find()
+        const result =await cursor.toArray()
+        res.send(result)
+    });
+
+    app.get('/Job/:id', async(req,res)=>{
+        const id= req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await jobsCollection.findOne(query)
+        res.send(result)
+
+    });
+
+    app.get('/application', async(req,res)=>{
+        const email =req.query.email;
+
+        const query ={
+            applicant: email
+        }
+        const result =await applicationsCollection.find(query).toArray();
+        for(const app of result){
+            const jobId =app.jobId;
+            const jobQuery = {_id: new ObjectId(jobId)}
+            const job =await jobsCollection.findOne(jobQuery);
+            app.company =job.company
+            app.title =job.title;
+            app.company_logo =job.company_logo
+        }
+        res.send(result)
+    })
+
+    // job application api
+    app.post('/application',async(req,res)=>{
+        const application =req.body;
+        console.log(application)
+        const result =await applicationsCollection.insertOne(application);
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
